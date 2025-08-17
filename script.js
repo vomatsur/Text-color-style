1fbe85ff95f816a5b190d7f0607fe40c93991041
vomatsur <bahriyorovbahrom@gmail.com>
2025-08-17 20:56:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let h1 = document.querySelector('h1');
+
+
+new Typed(h1, {
+    strings:['Text Gradients','Hello'],
+    typeSpeed:50,
+    backSpeed:50,
+    startDelay:100,
+    loop:true,
+})
+
